b2afdf3f36ee4ac53e5f84f6a73d367b152c782d
Camwp <camwpedro@gmail.com>
2023-01-03 14:01:22 -0800

    prep 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+        int intGuess = -1;
+        int numTries = 0;
+        string tries = "tries";
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        Console.WriteLine($"Random number generated successfully.{magicNumber}");
+
+        while (intGuess != magicNumber)
+        {
+            numTries += 1;
+            Console.Write("What is your guess? ");
+            intGuess = int.Parse(Console.ReadLine());
+
+            if (intGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (intGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+        if (numTries == 1)
+        {
+            tries = "try";
+        }
+
+        Console.WriteLine($"You guessed it! It took {numTries} {tries}");
+        
     }
 }
