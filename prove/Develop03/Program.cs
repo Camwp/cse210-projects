818d9c69fd99460da4d16a907cc667587a5f0133
Camwp <camwpedro@gmail.com>
2023-02-01 12:41:15 -0800

    Working Project

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,8 +3,8 @@ using System;
 class Program
 {
     //Set program as active bool
-    static bool _active = true;
-    static string _fullScript;
+    private static bool _active = true;
+    private static string _fullScript;
 
     //Start main class
     static void Main(string[] args)
@@ -15,7 +15,9 @@ class Program
         Prompt p = new Prompt();
         Console.Clear();
         
+        //Get full scripture (Reference and text) in format "{_scriptReference}. {_scriptText}"
         _fullScript = s.GetScripture(); 
+        //Strip the DOT and print the scripture for the first time
         Console.WriteLine(_fullScript.Replace(".", string.Empty));
 
         //Main While loop
@@ -23,13 +25,23 @@ class Program
         {
             //Get user input
             string userInput = p.GetInput();
+            //Clear the screen after getting the user input and prepare for updated scripture or quit
             Console.Clear();
 
             //Check their input
             if (userInput == "")
             {
-                _fullScript = u.UpdateScripture(_fullScript);
-                Console.WriteLine(_fullScript);
+                if (u.UpdateScripture(_fullScript) == "COMPLETED")
+                {
+                    _active = false;
+                }
+                else
+                {
+                    //Update the scripture and return it to the _fullScript variable
+                    _fullScript = u.UpdateScripture(_fullScript);
+                    //Print the newly updated scripture text with reference
+                    Console.WriteLine(_fullScript.Replace(".", string.Empty));
+                }
             }
             else if (userInput == "quit")
             {
@@ -37,10 +49,11 @@ class Program
             }
             else
             {
+                //Rewrite the scripture if an invalid key is entered with a message 
+                //telling the user to input a valid key
                 Console.WriteLine(_fullScript.Replace(".", string.Empty));
                 Console.WriteLine("Please enter a valid input.");
             }
-            
         }
     }
 }
--- prove/Develop03/UpdateScripture.cs
+++ prove/Develop03/UpdateScripture.cs
@@ -1,19 +1,46 @@
+using System.Text.RegularExpressions;
+
 class Update
 {
-    private static List<string> text = new List<string>();
+    private static List<string> result = new List<string>();
     public string UpdateScripture(string rawScript)
     {
-        Random rand = new Random();
         string reference = rawScript.Split(".")[0].Trim();
         string scriptText = rawScript.Split(".")[1].Trim();
 
         //MODIFY SCRIPTTEXT TO REMOVE RANDOM WORDS AND CONTINUE
-        //List<string> result = scriptText.ToList();
+        //Convert script text to list separated by commas
+        List<string> result = scriptText.Split(" ").ToList();
+        int isEmpty = Regex.Matches(scriptText,@"[a-zA-Z]").Count;
+        int i = -1;
+        
 
-        //Console.WriteLine(result);
+        if (isEmpty >= 1)
+        {
+            //For every word in the list of text
+            foreach (string word in result.ToList())
+            {
+                i += 1;
+                //Generate a random number 1-5
+                Random rand = new Random();
+                int rndnum = rand.Next(0,5);
+
+                if (rndnum == 1 || rndnum == 3)
+                {
+                    var res=new Regex("\\S").Replace(word,"_");
+                    result[i] = res;
+                }
+            }
+        }
+        else if (isEmpty == 0)
+        {
+            return "COMPLETED";
+        }
+        string resultString = string.Join(" ",result);
+        
         
         //Update full scripture string with new random missing words
-        string updatedScripture = $"{reference} {scriptText}";
+        string updatedScripture = $"{reference}. {resultString}";
 
         return updatedScripture;
     }
