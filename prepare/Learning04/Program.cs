9fc10025eaad16d5d8fd40591898a6b4ec6fd8ca
Camwp <camwpedro@gmail.com>
2023-02-16 08:07:45 -0800

    final

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,11 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a base "Assignment" object
+
         Assignment assign1 = new Assignment("Cameron Pedro", "Programming");
         Console.WriteLine(assign1.GetSummary());
 
-        // Now create the derived class assignments
         MathAssignment assign2 = new MathAssignment("Cash Pedro", "Division", "7.3", "8-19");
         Console.WriteLine(assign2.GetSummary());
         Console.WriteLine(assign2.GetHomeworkList());
