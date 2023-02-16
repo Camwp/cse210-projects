77ce55c52d4c75cdfcfb25bbad057baee1aa5fcc
Cameron Pedro <camwcats@gmail.com>
2023-02-15 21:21:11 -0800

    complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base "Assignment" object
+        Assignment assign1 = new Assignment("Cameron Pedro", "Programming");
+        Console.WriteLine(assign1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment assign2 = new MathAssignment("Cash Pedro", "Division", "7.3", "8-19");
+        Console.WriteLine(assign2.GetSummary());
+        Console.WriteLine(assign2.GetHomeworkList());
+
+        WritingAssignment assign3 = new WritingAssignment("Macey Goetzel", "World War II", "The Aftermath of World War II");
+        Console.WriteLine(assign3.GetSummary());
+        Console.WriteLine(assign3.GetWritingInformation());
     }
 }
